fa5de37f8fd39489a5dfc49b7518f58f428324fe
Zougui <zougui.dev@protonmail.com>
2025-03-20 20:52:38 +0100

    fix: increased delays

--- src/game-features/view.ts
+++ src/game-features/view.ts
@@ -10,75 +10,76 @@ export const navigateViews: Record<ViewName, () => Promise<void>> = {
     for (const _ of views) {
       await press({ key: hotkeys.escape });
       store.trigger.popView();
+      await sleep(1000);
     }
-
-    await sleep(200);
   },
   town: async () => {
     await press({ key: hotkeys.town });
     store.trigger.navigateView({ view: 'town' });
-    await sleep(500);
+    await sleep(1000);
   },
   alchemist: async () => {
     await press({ key: hotkeys.alchemist });
     store.trigger.navigateView({ view: 'alchemist' });
-    await sleep(500);
+    await sleep(1000);
   },
   oracle: async () => {
     await press({ key: hotkeys.oracle });
     store.trigger.navigateView({ view: 'oracle' });
-    await sleep(500);
+    await sleep(1000);
   },
   guardians: async () => {
     await press({ key: hotkeys.guardians });
     store.trigger.navigateView({ view: 'guardians' });
-    await sleep(500);
+    await sleep(1000);
   },
   library: async () => {
     await press({ key: hotkeys.library });
     store.trigger.navigateView({ view: 'library' });
-    await sleep(500);
+    await sleep(1000);
   },
   map: async () => {
     await press({ key: hotkeys.map });
+    // wait for animation to finish
+    await sleep(150);
+    await click({ left: '96%', top: '45%' });
     store.trigger.navigateView({ view: 'map' });
-    await sleep(500);
+    await sleep(1000);
   },
   engineerNavigation: async () => {
     await navigateViews.town();
     // engineer building button
     await click({ left: '65%', top: '80%' });
     store.trigger.navigateView({ view: 'engineerNavigation', isDialog: true });
-    await sleep(500);
+    await sleep(1000);
   },
   engineer: async () => {
     await navigateViews.engineerNavigation();
     // engineer panel button
     await click({ left: '30%', top: '50%' });
     store.trigger.navigateView({ view: 'engineer' });
-    await sleep(500);
+    await sleep(1000);
   },
   campaign: async () => {
-    await navigateViews.map();
+    await press({ key: hotkeys.map });
     // wait for animation to finish
     await sleep(150);
     await click({ left: '96%', top: '55%' });
-    store.trigger.popView();
     store.trigger.navigateView({ view: 'map' });
-    await sleep(500);
+    await sleep(1000);
   },
   guild: async () => {
     // guild icon
     await click({ left: '97%', top: '42%' });
     store.trigger.navigateView({ view: 'guild' });
-    await sleep(500);
+    await sleep(1000);
   },
   guildExpeditions: async () => {
     await navigateViews.guild();
     // expedition building
     await click({ left: '20%', top: '32%' });
     store.trigger.navigateView({ view: 'guildExpeditions' });
-    await sleep(500);
+    await sleep(1000);
   },
 };
 
