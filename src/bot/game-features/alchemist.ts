450ba2b5e51030d2417208c0501397e255702bb1
Zougui <zougui.dev@protonmail.com>
2025-03-29 22:44:59 +0100

    fixed exotic coin experiment

--- src/bot/game-features/alchemist.ts
+++ src/bot/game-features/alchemist.ts
@@ -12,16 +12,16 @@ const claimAndRestart = ({ name, left }: { name: string; left: `${number}%`; }) 
       height: '3.5%',
     }),
     Effect.flatMap(texts => Effect.if(
-      texts.some(text => text.content.toLowerCase() === 'speed up'),
+      texts.some(text => ['free', 'claim'].includes(text.content.toLowerCase())),
       {
-        onTrue: () => Effect.logDebug(`Experiment ${name} has not yet finished`),
-        onFalse: () => pipe(
+        onTrue: () => pipe(
           Effect.logDebug(`Claiming experiment ${name}`),
           Effect.tap(() => click({ left, top: '75%' })),
           Effect.tap(() => Effect.logDebug(`Starting experiment ${name}`)),
           Effect.tap(() => click({ left, top: '75%' })),
           Effect.tap(() => Effect.log(`Handled experiment ${name}`)),
         ),
+        onFalse: () => Effect.logDebug(`Experiment ${name} has not yet finished`),
       },
     )),
   );
@@ -33,7 +33,7 @@ export const handleExperiments = () => {
     Effect.tap(() => goTo.alchemist()),
 
     Effect.tap(() => claimAndRestart({ name: 'blood', left: '45%' })),
-    Effect.tap(() => claimAndRestart({ name: 'exotic coins', left: '81%' })),
+    Effect.tap(() => claimAndRestart({ name: 'exotic coins', left: '82.5%' })),
 
     Effect.withSpan('experiments'),
     Effect.withLogSpan('experiments'),
